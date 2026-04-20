6bce6616de2f84d2685ae04442f181770c8f40e7
maxdhero1 <maxdhero1@gmail.com>
2026-04-20 21:43:16 +0100

    practise code for express.js

---
+++ server.js
@@ -0,0 +1,33 @@
+//step 1:import the express module
+
+const express = require('express');
+
+//step 2:create an express application instance
+const app = express();
+const studentInfo=[
+    {name:"Alice",age:20,grade:"A"},
+    {name:"Bob", age:21, grade:"B"},
+    {name:"Maxwell", age:22, grade:"C"}
+]
+app.get("/all-data",(req, res)=>{
+    res.json(studentInfo[2]);
+})
+//step 3: Define a route handler for GET request to /
+
+app.get('/', function(req, res){
+    res.send('Hello, world! welcome to express.js!');
+});
+
+//step 4:start listening on port 3000
+app.listen(3000, function(){
+    console.log('server is up and running now')
+});
+app.get('/About',(req, res)=>{
+    res.send("this is about page")
+})
+app.get('/Contact',(req, res)=>{
+    res.send("this is my contact page")
+})
+app.listen(3000, function(){
+    console.log("server is up and running")
+})
